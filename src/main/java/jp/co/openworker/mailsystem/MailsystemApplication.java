994dc3f822691737347d483e52053bdb6f7e820c
maki567 <maki.takuya@ccaweb.co.jp>
2021-01-28 15:05:21 +0900

    gradle初期設定

---
+++ src/main/java/jp/co/openworker/mailsystem/MailsystemApplication.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsystemApplication.class, args);
+	}
+
+}
