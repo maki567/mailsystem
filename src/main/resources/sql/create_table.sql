8326532500feda79f16d6d9fd45f1d1298d8b7d8
maki567 <maki.takuya@ccaweb.co.jp>
2021-02-08 16:36:57 +0900

    create_table.sqlにregister_addressテーブルを作成 register_address.htmlcssの修正

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -12,3 +12,14 @@ CREATE TABLE mst_user (
  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp(),
  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 );
+
+CREATE TABLE mst_address (
+ id int(11) PRIMARY KEY AUTO_INCREMENT,
+ campany_name VARCHAR(32) NOT NULL UNIQUE,
+ staff_name VARCHAR(16) NOT NULL,
+ mail_address VARCHAR(32) NOT NULL UNIQUE,
+ company_address VARCHAR(255),
+ phone_number VARCHAR(16),
+ created_at TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+ updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+);
