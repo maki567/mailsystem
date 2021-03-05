3a9e8ed4d60511a7f09be4b3dac87882d988356a
ok-com <63033202+ok-com@users.noreply.github.com>
2021-03-05 13:08:56 +0900

    login.html,login.css,LoginControllerを作成。

---
+++ src/main/java/jp/co/openworker/mailsystem/controller/LoginController.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/mailsystem/login")
+	public String login() {
+		return "login";
+	}
+}
