885c84279670f9d571d6249cc649a4ffcd8dd23d
ok-com <63033202+ok-com@users.noreply.github.com>
2021-03-05 13:26:52 +0900

    dialogconfig.jsのuserNameをmailAddressに変更。

--- src/main/resources/static/js/dialogConfig.js
+++ src/main/resources/static/js/dialogConfig.js
@@ -52,10 +52,7 @@ const dialogConfig = {
 					let jsonString = {
 						'familyName': $('table#register input[name=familyName]').val(),
 						'firstName': $('table#register input[name=firstName]').val(),
-						'familyNameKana': $('table#register input[name=familyNameKana]').val(),
-						'firstNameKana': $('table#register input[name=firstNameKana]').val(),
-						'gender': $('table#register input[name=gender]:checked').val() == '男性' ? '0' : '1',
-						'userName': $('table#register input[name=userName]').val(),
+						'mailAddress': $('table#register input[name=mailAddress]').val(),
 						'password': $('table#register input[name=password]').val()
 					};
 					$.ajax({
@@ -71,10 +68,7 @@ const dialogConfig = {
 						$('#checkOK').addClass('hidden');
 						$('table#register input[name=familyName]').val('');
 						$('table#register input[name=firstName]').val('');
-						$('table#register input[name=familyNameKana]').val('');
-						$('table#register input[name=firstNameKana]').val('');
-						($('table#register input[name=gender]')[0]).checked = true;
-						$('table#register input[name=userName]').val('');
+						$('table#register input[name=mailAddress]').val('');
 						$('table#register input[name=password]').val('');
 					}, () => {
 						alert('Error: ajax connection failed.');
