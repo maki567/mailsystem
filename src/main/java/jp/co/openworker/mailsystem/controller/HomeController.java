ab077e92317398cc79c879de8e18b3af342729c7
ok-com <63033202+ok-com@users.noreply.github.com>
2021-03-08 13:57:00 +0900

    home.html,HomeController作成。新規ユーザー登録画面の登録完了後に、ログイン画面へのリンク追加。

---
+++ src/main/java/jp/co/openworker/mailsystem/controller/HomeController.java
@@ -0,0 +1,13 @@
+package jp.co.openworker.mailsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/mailsystem/home")
+	public String home() {
+		return "home";
+	}
+}
