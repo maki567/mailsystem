cf8431d0ecd1d6bb0c1339025fbbef3c45e8d00c
ok-com <63033202+ok-com@users.noreply.github.com>
2021-03-10 16:21:10 +0900

    login可能。

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -1,3 +1,6 @@
 --mst_user
 INSERT INTO mst_user(mail_address,password,family_name,first_name)
 VALUES('yamada@gmail.com','taropw','山田','太郎');
+
+INSERT INTO mst_address(company_name,staff_name,mail_address,company_address,phone_number)
+VALUES('qw','taro','w@w','rt','020');
