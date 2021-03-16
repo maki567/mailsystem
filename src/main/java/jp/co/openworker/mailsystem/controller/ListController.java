3c2d1b9751f12c7976af412318e05ee2e3080822
ok-com <63033202+ok-com@users.noreply.github.com>
2021-03-16 13:22:08 +0900

    連絡先登録画面のレイアウト調整。

--- src/main/java/jp/co/openworker/mailsystem/controller/ListController.java
+++ src/main/java/jp/co/openworker/mailsystem/controller/ListController.java
@@ -1,10 +1,14 @@
 package jp.co.openworker.mailsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.openworker.mailsystem.model.domain.MstAddress;
+import jp.co.openworker.mailsystem.model.form.SearchForm;
 import jp.co.openworker.mailsystem.model.session.LoginSession;
 
 @Controller
@@ -19,4 +23,15 @@ public class ListController {
 		m.addAttribute("loginSession", loginSession);
 		return "address_list";
 	}
+	
+	@RequestMapping("/search")
+	public String index(SearchForm f, Model m) {
+		List<MstAddress> companyName;
+
+		String keywords = f.getKeywords().replaceAll("Å@", " ").replaceAll("\\s{2,}", " ").trim();
+
+		return "address_list";
+		
+	}
+	
 }
