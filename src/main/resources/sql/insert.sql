61cd978c5a93dd96f9647a1d9e7a4529ce101ce2
maki567 <maki.takuya@ccaweb.co.jp>
2021-03-23 17:29:43 +0900

    ホーム画面DB複数表示可能

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -10,10 +10,10 @@ INSERT INTO mst_address(id,company_name,company_address,corporation,company_numb
 VALUES('3','a2','a','0','0','a','a@a','0');
 
 INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('業務経歴書の件でのご相談','田中太郎','ABC','2021/03/03 12:17');
+VALUES('業務経歴書の件でのご相談1','田中太郎','ABC','2021/03/03 12:17');
 
 INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('1','1','1','1');
+VALUES('業務経歴書の件でのご相談2','山田太郎','DEF','2022/04/04 12:17');
 
 INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('2','2','2','2');
+VALUES('業務経歴書の件でのご相談3','鈴木花子','GHI','2023/05/05 12:17');
