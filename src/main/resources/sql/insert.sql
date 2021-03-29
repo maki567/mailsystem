330333c306433f2234e8ca20a62f75f390e384a6
maki567 <maki.takuya@ccaweb.co.jp>
2021-03-29 14:36:28 +0900

    insert.sql　mst_address　会社50個追加

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -103,11 +103,124 @@ VALUES('49','aa','a','0','0','a','a@a','0');
 INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
 VALUES('50','aaaaa1','a','1','0','a','a@a','0');
 
-INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('業務経歴書の件でのご相談1','田中太郎','ABC','2021/03/03 12:17');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('51','ba','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('52','ba1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('53','ba2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('54','bg','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('55','bg1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('56','bah2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('57','bar','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('58','bar1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('59','ba2h','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('60','besa','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('61','bagr1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('62','ba2t','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('63','ba4ht','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('64','bag1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('65','ba52','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('66','bak','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('67','bau1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('68','bali2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('69','bap','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('70','ba41','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('71','baa2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('72','baf','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('73','bar1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('74','ba4t2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('75','bac','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('76','bav1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('77','ba2f','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('78','bae','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('79','ba41','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('80','bavv2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('81','babb','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('82','ba1bbn','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('83','ba2n','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('84','bna','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('85','ban1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('86','ban2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('87','bannnn','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('88','bavc1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('89','bacc2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('90','bac','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('91','baccc1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('92','baxx2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('93','bax','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('94','ba1x','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('95','baz2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('96','bazz','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('97','bazzz1','a','1','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('98','baaa2','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('99','baa','a','0','0','a','a@a','0');
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
+VALUES('100','baaaaa1','a','1','0','a','a@a','0');
 
 INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('業務経歴書の件でのご相談2','山田太郎','DEF','2022/04/04 12:17');
+VALUES('業務経歴書の件でのご相談1','田中太郎1','ABC1','2021/03/03 12:17');
 
 INSERT INTO mail_history(subject,to_staff,to_company,day_time)
-VALUES('業務経歴書の件でのご相談3','鈴木花子','GHI','2023/05/05 12:17');
+VALUES('業務経歴書の件でのご相談2','田中太郎2','ABC2','2021/03/03 12:17');
+
+INSERT INTO mail_history(subject,to_staff,to_company,day_time)
+VALUES('業務経歴書の件でのご相談3','田中太郎3','ABC3','2021/03/03 12:17');
+
+INSERT INTO mail_history(subject,to_staff,to_company,day_time)
+VALUES('業務経歴書の件でのご相談4','田中太郎4','ABC4','2021/03/03 12:17');
+
+INSERT INTO mail_history(subject,to_staff,to_company,day_time)
+VALUES('業務経歴書の件でのご相談5','田中太郎5','ABC5','2021/03/03 12:17');
+
+INSERT INTO mail_history(subject,to_staff,to_company,day_time)
+VALUES('業務経歴書の件でのご相談6','田中太郎6','ABC6','2021/03/03 12:17');
+
+INSERT INTO mail_history(subject,to_staff,to_company,day_time)
+VALUES('業務経歴書の件でのご相談7','田中太郎7','ABC7','2021/03/03 12:17');
