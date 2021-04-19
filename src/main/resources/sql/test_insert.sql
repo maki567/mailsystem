2186d643a73314a04bee561d7cbb2807fc15525d
ok-com <63033202+ok-com@users.noreply.github.com>
2021-04-19 09:30:13 +0900

    連絡先登録入力可能文字制限変更 会社名32→16　所在地64→32

--- src/main/resources/sql/test_insert.sql
+++ src/main/resources/sql/test_insert.sql
@@ -5,3 +5,9 @@ INSERT INTO mst_address(id,company_name,company_address,corporation,company_numb
 VALUES('2','あtest','','1','','b','b@b','');
 INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1)
 VALUES('3','いtesｔ','','0','','c','c@c','');
+
+
+
+//連絡先編集画面
+INSERT INTO mst_address(id,company_name,company_address,corporation,company_number,staff_name1,staff_mail1,staff_number1,staff_name2,staff_mail2,staff_number2,staff_name3,staff_mail3,staff_number3,staff_name4,staff_mail4,staff_number4,staff_name5,staff_mail5,staff_number5,staff_name6,staff_mail6,staff_number6,staff_name7,staff_mail7,staff_number7,staff_name8,staff_mail8,staff_number8,staff_name9,staff_mail9,staff_number9,staff_name10,staff_mail10,staff_number10)
+VALUES('1','Test','東京都','0','03-0000-0000','A','a@a','080-1111-1111','B','b@b','080-2222-2222','C','c@c','080-3333-3333','D','d@d','080-4444-4444','E','e@e','080-5555-5555','F','f@f','080-6666-6666','G','g@g','080-7777-7777','H','h@h','080-8888-8888','I','i@i','080-9999-9999','J','j@j','080-0000-0000');
