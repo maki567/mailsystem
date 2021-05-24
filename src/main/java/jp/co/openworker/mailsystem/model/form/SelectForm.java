c3a02c14f165648c761a984ae11757138e3abda2
ok-com <63033202+ok-com@users.noreply.github.com>
2021-05-24 17:20:07 +0900

    SelectForm修正完了。

--- src/main/java/jp/co/openworker/mailsystem/model/form/SelectForm.java
+++ src/main/java/jp/co/openworker/mailsystem/model/form/SelectForm.java
@@ -11,8 +11,51 @@ public class SelectForm implements Serializable {
 	private int corporation1;
 	private String staffName1;
 	private String staffMail1;
+	private int userId2;
+	private String companyName2;
+	private int corporation2;
 	private String staffName2;
 	private String staffMail2;
+	private int userId3;
+	private String companyName3;
+	private int corporation3;
+	private String staffName3;
+	private String staffMail3;
+	private int userId4;
+	private String companyName4;
+	private int corporation4;
+	private String staffName4;
+	private String staffMail4;
+	private int userId5;
+	private String companyName5;
+	private int corporation5;
+	private String staffName5;
+	private String staffMail5;
+	private int userId6;
+	private String companyName6;
+	private int corporation6;
+	private String staffName6;
+	private String staffMail6;
+	private int userId7;
+	private String companyName7;
+	private int corporation7;
+	private String staffName7;
+	private String staffMail7;
+	private int userId8;
+	private String companyName8;
+	private int corporation8;
+	private String staffName8;
+	private String staffMail8;
+	private int userId9;
+	private String companyName9;
+	private int corporation9;
+	private String staffName9;
+	private String staffMail9;
+	private int userId10;
+	private String companyName10;
+	private int corporation10;
+	private String staffName10;
+	private String staffMail10;
 	
 	public int getId() {
 		return id;
@@ -50,6 +93,25 @@ public class SelectForm implements Serializable {
 	public void setStaffMail1(String staffMail1) {
 		this.staffMail1 = staffMail1;
 	}
+
+	public int getUserId2() {
+		return userId2;
+	}
+	public void setUserId2(int userId2) {
+		this.userId2 = userId2;
+	}
+	public String getCompanyName2() {
+		return companyName2;
+	}
+	public void setCompanyName2(String companyName2) {
+		this.companyName2 = companyName2;
+	}
+	public int getCorporation2() {
+		return corporation2;
+	}
+	public void setCorporation2(int corporation2) {
+		this.corporation2 = corporation2;
+	}
 	public String getStaffName2() {
 		return staffName2;
 	}
@@ -62,4 +124,253 @@ public class SelectForm implements Serializable {
 	public void setStaffMail2(String staffMail2) {
 		this.staffMail2 = staffMail2;
 	}
+	
+	public int getUserId3() {
+		return userId3;
+	}
+	public void setUserId3(int userId3) {
+		this.userId3 = userId3;
+	}
+	public String getCompanyName3() {
+		return companyName3;
+	}
+	public void setCompanyName3(String companyName3) {
+		this.companyName3 = companyName3;
+	}
+	public int getCorporation3() {
+		return corporation3;
+	}
+	public void setCorporation3(int corporation3) {
+		this.corporation3 = corporation3;
+	}
+	public String getStaffName3() {
+		return staffName3;
+	}
+	public void setStaffName3(String staffName3) {
+		this.staffName3 = staffName3;
+	}
+	public String getStaffMail3() {
+		return staffMail3;
+	}
+	public void setStaffMail3(String staffMail3) {
+		this.staffMail3 = staffMail3;
+	}
+
+	public int getUserId4() {
+		return userId4;
+	}
+	public void setUserId4(int userId4) {
+		this.userId4 = userId4;
+	}
+	public String getCompanyName4() {
+		return companyName4;
+	}
+	public void setCompanyName4(String companyName4) {
+		this.companyName4 = companyName4;
+	}
+	public int getCorporation4() {
+		return corporation4;
+	}
+	public void setCorporation4(int corporation4) {
+		this.corporation4 = corporation4;
+	}
+	public String getStaffName4() {
+		return staffName4;
+	}
+	public void setStaffName4(String staffName4) {
+		this.staffName4 = staffName4;
+	}
+	public String getStaffMail4() {
+		return staffMail4;
+	}
+	public void setStaffMail4(String staffMail4) {
+		this.staffMail4 = staffMail4;
+	}
+	
+	public int getUserId5() {
+		return userId5;
+	}
+	public void setUserId5(int userId5) {
+		this.userId5 = userId5;
+	}
+	public String getCompanyName5() {
+		return companyName5;
+	}
+	public void setCompanyName5(String companyName5) {
+		this.companyName5 = companyName5;
+	}
+	public int getCorporation5() {
+		return corporation5;
+	}
+	public void setCorporation5(int corporation5) {
+		this.corporation5 = corporation5;
+	}
+	public String getStaffName5() {
+		return staffName5;
+	}
+	public void setStaffName5(String staffName5) {
+		this.staffName5 = staffName5;
+	}
+	public String getStaffMail5() {
+		return staffMail5;
+	}
+	public void setStaffMail5(String staffMail5) {
+		this.staffMail5 = staffMail5;
+	}
+
+	public int getUserId6() {
+		return userId6;
+	}
+	public void setUserId6(int userId6) {
+		this.userId6 = userId6;
+	}
+	public String getCompanyName6() {
+		return companyName6;
+	}
+	public void setCompanyName6(String companyName6) {
+		this.companyName6 = companyName6;
+	}
+	public int getCorporation6() {
+		return corporation6;
+	}
+	public void setCorporation6(int corporation6) {
+		this.corporation6 = corporation6;
+	}
+	public String getStaffName6() {
+		return staffName6;
+	}
+	public void setStaffName6(String staffName6) {
+		this.staffName6 = staffName6;
+	}
+	public String getStaffMail6() {
+		return staffMail6;
+	}
+	public void setStaffMail6(String staffMail6) {
+		this.staffMail6 = staffMail6;
+	}
+	
+	public int getUserId7() {
+		return userId7;
+	}
+	public void setUserId7(int userId7) {
+		this.userId7 = userId7;
+	}
+	public String getCompanyName7() {
+		return companyName7;
+	}
+	public void setCompanyName7(String companyName7) {
+		this.companyName7 = companyName7;
+	}
+	public int getCorporation7() {
+		return corporation7;
+	}
+	public void setCorporation7(int corporation7) {
+		this.corporation7 = corporation7;
+	}
+	public String getStaffName7() {
+		return staffName7;
+	}
+	public void setStaffName7(String staffName7) {
+		this.staffName7 = staffName7;
+	}
+	public String getStaffMail7() {
+		return staffMail7;
+	}
+	public void setStaffMail7(String staffMail7) {
+		this.staffMail7 = staffMail7;
+	}
+
+	public int getUserId8() {
+		return userId8;
+	}
+	public void setUserId8(int userId8) {
+		this.userId8 = userId8;
+	}
+	public String getCompanyName8() {
+		return companyName8;
+	}
+	public void setCompanyName8(String companyName8) {
+		this.companyName8 = companyName8;
+	}
+	public int getCorporation8() {
+		return corporation8;
+	}
+	public void setCorporation8(int corporation8) {
+		this.corporation8 = corporation8;
+	}
+	public String getStaffName8() {
+		return staffName8;
+	}
+	public void setStaffName8(String staffName8) {
+		this.staffName8 = staffName8;
+	}
+	public String getStaffMail8() {
+		return staffMail8;
+	}
+	public void setStaffMail8(String staffMail8) {
+		this.staffMail8 = staffMail8;
+	}
+	
+	public int getUserId9() {
+		return userId9;
+	}
+	public void setUserId9(int userId9) {
+		this.userId9 = userId9;
+	}
+	public String getCompanyName9() {
+		return companyName9;
+	}
+	public void setCompanyName9(String companyName9) {
+		this.companyName9 = companyName9;
+	}
+	public int getCorporation9() {
+		return corporation9;
+	}
+	public void setCorporation9(int corporation9) {
+		this.corporation9 = corporation9;
+	}
+	public String getStaffName9() {
+		return staffName9;
+	}
+	public void setStaffName9(String staffName9) {
+		this.staffName9 = staffName9;
+	}
+	public String getStaffMail9() {
+		return staffMail9;
+	}
+	public void setStaffMail9(String staffMail9) {
+		this.staffMail9 = staffMail9;
+	}
+
+	public int getUserId10() {
+		return userId10;
+	}
+	public void setUserId10(int userId10) {
+		this.userId10 = userId10;
+	}
+	public String getCompanyName10() {
+		return companyName10;
+	}
+	public void setCompanyName10(String companyName10) {
+		this.companyName10 = companyName10;
+	}
+	public int getCorporation10() {
+		return corporation10;
+	}
+	public void setCorporation10(int corporation10) {
+		this.corporation10 = corporation10;
+	}
+	public String getStaffName10() {
+		return staffName10;
+	}
+	public void setStaffName10(String staffName10) {
+		this.staffName10 = staffName10;
+	}
+	public String getStaffMail10() {
+		return staffMail10;
+	}
+	public void setStaffMail10(String staffMail10) {
+		this.staffMail10 = staffMail10;
+	}
+	
 }
