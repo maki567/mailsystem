aec94fcf33cbc4794038d3c912d2ed0608146e4c
ok-com <63033202+ok-com@users.noreply.github.com>
2021-05-24 16:39:58 +0900

    SelectAddressMapper修正完了。

--- src/main/java/jp/co/openworker/mailsystem/model/mapper/SelectAddressMapper.java
+++ src/main/java/jp/co/openworker/mailsystem/model/mapper/SelectAddressMapper.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import jp.co.openworker.mailsystem.model.domain.SelectAddress;
 
@@ -36,4 +35,92 @@ public interface SelectAddressMapper {
 				+ ")")
 	@Options(useGeneratedKeys=true, keyProperty="id")
 	int insert2(SelectAddress address);
+	
+	@Insert("INSERT INTO select_address ("
+			+ "user_id, company_name, corporation,"
+			+ "staff_name, staff_mail"
+			+ ") "
+			+ "VALUES ("
+			+ "#{userId3}, #{companyName3}, #{corporation3},"
+			+ "#{staffName3}, #{staffMail3}"
+			+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert3(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+			+ "user_id, company_name, corporation,"
+			+ "staff_name, staff_mail"
+			+ ") "
+			+ "VALUES ("
+			+ "#{userId4}, #{companyName4}, #{corporation4},"
+			+ "#{staffName4}, #{staffMail4}"
+			+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert4(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId5}, #{companyName5}, #{corporation5},"
+		+ "#{staffName5}, #{staffMail5}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert5(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId6}, #{companyName6}, #{corporation6},"
+		+ "#{staffName6}, #{staffMail6}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert6(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId7}, #{companyName7}, #{corporation7},"
+		+ "#{staffName7}, #{staffMail7}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert7(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId8}, #{companyName8}, #{corporation8},"
+		+ "#{staffName8}, #{staffMail8}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert8(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId9}, #{companyName9}, #{corporation9},"
+		+ "#{staffName9}, #{staffMail9}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert9(SelectAddress address);
+
+	@Insert("INSERT INTO select_address ("
+		+ "user_id, company_name, corporation,"
+		+ "staff_name, staff_mail"
+		+ ") "
+		+ "VALUES ("
+		+ "#{userId10}, #{companyName10}, #{corporation10},"
+		+ "#{staffName10}, #{staffMail10}"
+		+ ")")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	int insert10(SelectAddress address);
 }
